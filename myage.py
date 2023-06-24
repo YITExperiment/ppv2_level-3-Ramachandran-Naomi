70ffc02a41280d8bd868560ecc7f523ab923d15a
Ramachandran-Naomi <135986215+Ramachandran-Naomi@users.noreply.github.com>
2023-06-24 20:26:54 +0530

    Add files via upload

---
+++ myage.py
@@ -0,0 +1,11 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age=14
+>>> print(age)
+14
+>>> weight=24
+>>> print(weight)
+24
+>>> height=147
+>>> print(height)
+147
